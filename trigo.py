6ffd90afdf581eb39e856277f0a10c383df84405
Kevin Dylla <kevinjandylla@gmail.com>
2017-10-06 10:39:00 +0200

    Update Kosinus,Tangens. usage() hinzugefugt

--- trigo.py
+++ trigo.py
@@ -34,7 +34,48 @@ def use():
         SinusWinkel = auswahl1G / auswahl1H
 
         print "\n Der Sinus des Winkels ist %s " % SinusWinkel
+# Kosinus
+    elif auswahl == 2 :
+        print ('''
+                    Kosinus soll berechnet werden
+
+                    Kosinus folgt dem Schema 
+                    
+                    Ankathete / Hypotenuse (b/c)
+
+                    Bitte geben sie sie die Werte ein
+
+                ''')
+        auswahl2A = float(raw_input("Geben sie die lange der Ankathete des Winkels an ~> "))
+        auswahl2H = float(raw_input("Jetzt die lange der Hypotenuse ~> "))
+        
+        # Rechnet den Sinus des Winkela
+        KosinusWinkel = auswahl2A / auswahl2H
+
+        print "\n Der Kosinus des Winkels ist %s " % KosinusWinkel
+
+#Tangens
+    elif auswahl == 3:
+        
+
+        print ('''
+                    Tangens soll berechnet werden
+
+                    Tangens folgt dem Schema 
+                    
+                    Gegenkathete / Ankathete (a/b)
+
+                    Bitte geben sie sie die Werte ein
+
+                ''')
+        auswahl3G = float(raw_input("Geben sie die lange der Gegenkathete  des Winkels an ~> "))
+        auswahl3A = float(raw_input("Jetzt die lange der Ankathete ~> "))
+        
+        # Rechnet den Sinus des Winkela
+        TangensWinkel = auswahl3G / auswahl3A
+
+        print "\n Der Tangens des Winkels ist %s " % TangensWinkel
 
 
-
+usage()
 use()
